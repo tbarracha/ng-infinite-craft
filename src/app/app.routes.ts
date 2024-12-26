4ea61b043f5cb58c5e8b0af1917dbbd1100a872b
Tiago Barracha <taigo.barracha@gmail.com>
2024-12-26 22:34:18 +0000

    page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageElementCraftComponent } from './pages/page-element-craft/page-element-craft.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: PageElementCraftComponent },
-    { path: '**', redirectTo: 'home' }
+    { path: '**', component: PageNotFoundComponent }
 ];
